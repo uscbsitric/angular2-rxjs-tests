2bb15445b55bba3bd743444ad9c2b87386dd92c6
Frederick Sandalo <frederick.sandalo@zoramteq.com>
2018-01-08 17:17:41 +0800

    Added SUCCESS, ERROR and COMPLETE catchers during XHR operations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -53,8 +53,9 @@ interface PutRequestPayload
 })
 export class AppComponent
 {
-  courses$: Observable<MockServerData[]>;
+  courses$: Observable<MockServerData>;
   requestURL = 'http://mockserver.local/mockserver.php';
+  requestURL2 = 'http://mockserver.local/mockserver2.php';
 
   putRequestPayload: PutRequestPayload;
 
@@ -68,7 +69,7 @@ export class AppComponent
     const key = 'someKey';
     const params = new HttpParams().set('orderBy', key)
                                        .set('limitToFirst', '11111111111');
-    const headers = new HttpHeaders().set('Content-Type', 'application/jsonDDDDD');
+    const headers = new HttpHeaders().set('Content-Type', 'application/json');
 
     this.putRequestPayload = {'courseListIcon': '.../main-page-logo-small-hat.png',
                               'description': 'Angular Tutorial For Beginners TEST',
@@ -77,17 +78,57 @@ export class AppComponent
                               'url': 'new-value-for-url'
                              };
 
-    // WORKING HTTP GET Code
+/*     // WORKING HTTP GET Code
     this.courses$ = this.http
                         .get<MockServerData>(this.requestURL, {'headers': headers, 'params': params})
                         .map(data => { const debugAnchor = '';
                                        return _.values(data);
                                      }
                             )
-                        .do(console.log);
+                        .do(console.log); */
     
 
 
-    /////this.courses$ = this.http.put<MockServerData>(this.requestURL, this.putRequestPayload, {headers});
+    /* this.courses$ = this.http.put<MockServerData>(this.requestURL, 
+                                                  this.putRequestPayload, {'headers': headers,
+                                                                           'params': params
+                                                                          }
+                                                 )
+                             .map( data => { const debugAnchor2 = '';
+                                             return _.values(data);
+                                           }
+                                 ); */
+                             /* .subscribe(// success
+                                           successResult => { let debugVal1 = '';
+                                                              debugVal1 = '111';
+                                                            },
+                                          // failure
+                                           errorResult => { let debugVal2 = '';
+                                                            debugVal2 = '222';
+                                                          },
+                                          // complete
+                                          () => { let debugVal3 = '';
+                                                  debugVal3 = '333';
+                                                }
+                                       ); */
+
+    /* THIS IS A WORKING PUT REQUEST WITH ERROR CATCHER
+    this.http.put(this.requestURL2,
+                  this.putRequestPayload,
+                  {'headers': headers,
+                    'params': params
+                  }
+                 )
+             .subscribe( result => { let debugVar1 = '';
+                                     debugVar1 = '111';
+                                   },
+                         error => { let debugVar2 = '';
+                                    debugVar2 = '222';
+                                  },
+                         () => { let debugVar3 = '';
+                                 debugVar3 = '333';
+                               }
+                       );
+    */
   }
 }
